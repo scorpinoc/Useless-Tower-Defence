4ff5040aba8472e3470413cd32dafd1b81acd73a
Aleksander <scorpinoc@bigmir.net>
2017-04-11 18:21:26 +0300

    GameEngine fully merged into GameEnginViewModel

--- Core/GameEngineViewModel.cs
+++ Core/GameEngineViewModel.cs
@@ -1,40 +1,67 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Windows.Input;
+using static Core.GameCell;
+using Timer = System.Timers.Timer;
 
 namespace Core
 {
     public sealed class GameEngineViewModel : INotifyPropertyChanged
     {
         #region fields
-        private readonly GameEngine _engine;
+        private GameStateOwner _stateOwner;
         #endregion
 
         #region properties
-
+        
         public event PropertyChangedEventHandler PropertyChanged;
 
         #region auto
+        private object Locker { get; }
+        private Timer Timer { get; }
+        private GameStateSaver GameStateSaver { get; }
+
         public ICommand NewGameCommand { get; }
         public ICommand BuildTowerCommand { get; }
         public ICommand NextLevelCommand { get; }
-        public ICommand LoadCommand { get; }
+        public ICommand UndoTurnCommand { get; }
         public ICommand SaveToFileCommand { get; }
         public ICommand LoadFromFileCommand { get; }
         #endregion
 
         #region delegate
-        public ObservableCollection<GameCell> Cells => _engine.Cells;
-        public GameState.Size GridSize => _engine.GridSize;
+        private GameStateOwner StateOwner
+        {
+            get { return _stateOwner; }
+            set
+            {
+                _stateOwner = value;
+                _stateOwner.PropertyChanged += (sender, args) =>
+                {
+                    // ReSharper disable once ExplicitCallerInfoArgument
+                    OnPropertyChanged(null);
+                    // ReSharper disable once ExplicitCallerInfoArgument
+                    _stateOwner.CurrentGameState.PropertyChanged += (o, eventArgs) => OnPropertyChanged(eventArgs.PropertyName);
+                };
+                // ReSharper disable once ExplicitCallerInfoArgument
+                OnPropertyChanged(null);
+            }
+        }
 
-        public int Gold => _engine.Gold;
-        public int Level => _engine.Level;
-        public int EnemiesLeft => _engine.EnemiesLeft;
-        public int CurrentTurn => _engine.CurrentTurn;
-        public int Lives => _engine.Lives;
+        private GameState GameState => StateOwner.CurrentGameState;
+
+        public ObservableCollection<GameCell> Cells => GameState.Cells;
+        public GameState.Size GridSize => GameState.GridSize;
+
+        public int Gold => GameState.Gold;
+        public int Level => GameState.Level;
+        public int EnemiesLeft => GameState.EnemiesLeft;
+        public int CurrentTurn => GameState.CurrentTurn;
+        public int Lives => GameState.Lives;
         #endregion
 
         #endregion
@@ -42,94 +69,204 @@ namespace Core
         #region constructors
         public GameEngineViewModel()
         {
-            _engine = new GameEngine();
-            _engine.PropertyChanged += (sender, args) => OnPropertyChanged(args.PropertyName);
+            Locker = new object();
+            Timer = new Timer(TimeSpan.FromSeconds(1).TotalMilliseconds);
+            Timer.Elapsed += (sender, args) =>
+            {
+                if (NextTurnCanExecute())
+                    NextTurnExecute();
+                else
+                    Timer.Stop();
+            };
+            GameStateSaver = new GameStateSaver();
+          
+            #region Commands
 
-            BuildTowerCommand = new ParameterisedDelegateCommand<GameCell>(_engine.BuildTower,
-                cell => cell?.State == GameCell.CellState.Empty, _engine);
-            NextLevelCommand = new VoidDelegateCommand(_engine.NextLevel,
-                o => _engine.EnemiesLeft == 0 && _engine.Lives > 0, _engine);
-            NewGameCommand = new VoidDelegateCommand(_engine.NewGame);
-            LoadCommand = new ParameterisedDelegateCommand<int>(_engine.UndoGameFor,
-                levels => _engine.SavesCount > levels - 1, _engine);
+            BuildTowerCommand = new ParameterisedDelegateCommand<GameCell>(BuildTowerExecute, BuildTowerCanExecute, this);
+            NextLevelCommand = new VoidDelegateCommand(NextLevelExecute, o => NextLevelCanExecute(), this);
+            // menu commands
+            NewGameCommand = new VoidDelegateCommand(NewGame);
+            UndoTurnCommand = new ParameterisedDelegateCommand<int>(UndoGameExecute, UndoGameCanExecute, this);
+
             const string file = "test.xml";
             // todo : auto start from pause if enemies left
-            SaveToFileCommand = new VoidDelegateCommand(() => _engine.SaveGameTo(file), o => _engine.EnemiesLeft == 0, _engine);
-            LoadFromFileCommand = new VoidDelegateCommand(() => _engine.LoadGameFrom(file));
+            SaveToFileCommand = new VoidDelegateCommand(() => SaveGameTo(file), o => EnemiesLeft == 0, this);
+            LoadFromFileCommand = new VoidDelegateCommand(() => LoadGameFrom(file));
+
+            #endregion
+            
+            NewGame();
         }
         #endregion
 
-        #region event handlers
+        #region methods
+
+        #region event invokers
+
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+
+        #region pure methods
+
+        private void NewGame()
+        {
+            Timer.Stop();
+
+            GameState gameState;
+            if (new Random().Next(2) == 0)
+                gameState = new GameState(
+                    new ObservableCollection<GameCell>
+                    {
+                        new GameCell(CellState.Empty), new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                        new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                        new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty), new GameCell(CellState.Empty),
+                        new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                        new GameCell(CellState.Empty), new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty)
+                    },
+                    new GameState.Size { Height = 5, Width = 4 });
+            else
+                gameState = new GameState(
+                    new ObservableCollection<GameCell>
+                    {
+                       new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                       new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                       new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                       new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty),
+                       new GameCell(CellState.Empty), new GameCell(CellState.Road),  new GameCell(CellState.Empty)
+                    },
+                    new GameState.Size { Height = 5, Width = 3 });
+
+            StateOwner = new GameStateOwner(gameState);
+            // ReSharper disable once ExplicitCallerInfoArgument
+            GameState.PropertyChanged += (o, eventArgs) => OnPropertyChanged(eventArgs.PropertyName);
+        }
+
+        private void NextTurnExecute()
+        {
+            lock (Locker)
+            {
+                var towers = GameState.Cells.Count(cell => cell.State == CellState.Tower);
+                GameState.EnemiesLeft -= towers + towers;
+                if (GameState.EnemiesLeft != 0)
+                {
+                    if (++GameState.CurrentTurn <= 10) return;
+                    --GameState.EnemiesLeft;
+                    --GameState.Lives;
+                    if (GameState.EnemiesLeft > 0 && GameState.Lives > 0)
+                        return;
+                }
+                GameState.CurrentTurn = 0;
+            }
+        }
+
+        private bool NextTurnCanExecute() => GameState.EnemiesLeft > 0 && GameState.Lives > 0;
+
+        private void BuildTowerExecute(GameCell cell)
+        {
+            cell.State = CellState.Tower;
+            GameState.Gold -= 80;
+        }
+
+        private bool BuildTowerCanExecute(GameCell cell) => cell.State == CellState.Empty && Gold >= 80;
+
+        private void NextLevelExecute()
+        {
+            StateOwner.Save();
+            GameState.EnemiesLeft = ++GameState.Level * GameState.Cells.Count(cell => cell.State == CellState.Road);
+            Timer.Start();
+        }
+
+        private bool NextLevelCanExecute() => GameState.EnemiesLeft == 0 && GameState.Lives > 0;
+
+        private void UndoGameExecute(int turns = 1)
+        {
+            for (; turns > 0; --turns)
+                StateOwner.Load();
+        }
+
+        private bool UndoGameCanExecute(int turns = 1) => turns >= 1 && turns <= StateOwner.SavesCount;
+
+        private void SaveGameTo(string file) => GameStateSaver.SaveTo(file, StateOwner.GameStates);
+
+        private void LoadGameFrom(string file)
+        {
+            Timer.Stop();
+            StateOwner = new GameStateOwner(GameStateSaver.LoadFrom(file));
+        }
+
+        #endregion
+
         #endregion
 
         #region inner classes
 
         private abstract class DelegateCommand<T> : ICommand
         {
-            #region fields
-            private readonly Predicate<T> _canExecute;
-            #endregion
-
             #region properties 
             public event EventHandler CanExecuteChanged;
+
+            private Predicate<T> CanExecutePredicate { get; }
             #endregion
 
             #region constructors
             protected DelegateCommand(Predicate<T> canExecute, INotifyPropertyChanged notifier)
             {
-                _canExecute = canExecute;
+                CanExecutePredicate = canExecute;
                 if (notifier == null) return;
                 var currentContext = SynchronizationContext.Current;
                 notifier.PropertyChanged +=
                     (sender, args) =>
-                        currentContext.Post(state => ((DelegateCommand<T>)state).CanExecuteUpdate(), this);
+                        currentContext.Post(state => ((DelegateCommand<T>)state).OnCanExecuteChanged(), this);
             }
             #endregion
 
             #region methods
 
-            public bool CanExecute(object parameter) => _canExecute?.Invoke((T)parameter) ?? false;
+            public bool CanExecute(object parameter) => CanExecutePredicate?.Invoke((T)parameter) ?? false;
 
             public abstract void Execute(object parameter);
-
-            protected void CanExecuteUpdate() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            
+            protected void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
 
             #endregion
+
         }
 
         private class VoidDelegateCommand : DelegateCommand<object>
         {
-            private readonly Action _execute;
+            private Action ExecuteAction { get; }
 
             public VoidDelegateCommand(Action execute, Predicate<object> canExecute = null, INotifyPropertyChanged notifier = null)
                 : base(canExecute ?? (o => true), notifier)
             {
-                _execute = execute;
+                ExecuteAction = execute;
             }
 
             public override void Execute(object parameter)
             {
-                _execute?.Invoke();
-                CanExecuteUpdate();
+                if (!CanExecute(parameter)) return;
+                ExecuteAction?.Invoke();
+                OnCanExecuteChanged();
             }
         }
 
         private class ParameterisedDelegateCommand<T> : DelegateCommand<T>
         {
-            private readonly Action<T> _execute;
+            private Action<T> ExecuteAction { get; }
 
             public ParameterisedDelegateCommand(Action<T> execute, Predicate<T> canExecute, INotifyPropertyChanged notifier = null)
                 : base(canExecute, notifier)
             {
-                _execute = execute;
+                ExecuteAction = execute;
             }
 
             public override void Execute(object parameter)
             {
-                _execute?.Invoke((T)parameter);
-                CanExecuteUpdate();
+                if (!CanExecute(parameter)) return;
+                ExecuteAction?.Invoke((T)parameter);
+                OnCanExecuteChanged();
             }
         }
         #endregion
